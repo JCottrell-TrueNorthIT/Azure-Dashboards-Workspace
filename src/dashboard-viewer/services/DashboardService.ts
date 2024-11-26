55b7ba9932ee41bd8e0d317ebb1f9cc84f113b9f
Joshua Cottrell <JCottrell@trueNorthIT.co.uk>
2024-11-26 15:26:50 +0000

    Fix for Tile Groups Replacement

--- src/dashboard-viewer/services/DashboardService.ts
+++ src/dashboard-viewer/services/DashboardService.ts
@@ -94,7 +94,7 @@ export class DashboardServiceClass implements IDashboardService {
     var tileYaml = yaml.dump(tile);
 
     for (const variable of group.variables) {
-      tileYaml = tileYaml.replaceAll(`{{${variable.name}}}`, variable.value);
+      tileYaml = tileYaml.replaceAll(`{{${variable.name}}}`, variable.value.replaceAll("\n", ""));
     }
 
     var injectedTile = yaml.load(tileYaml) as ITile;
