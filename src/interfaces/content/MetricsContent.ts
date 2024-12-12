c73e75b91efafd82adcbe77a9302cdaa24242f0b
Alex Radice <aradice@truenorthit.co.uk>
2024-12-12 08:54:06 +0000

    Improve schema for specifying filters

--- src/interfaces/content/MetricsContent.ts
+++ src/interfaces/content/MetricsContent.ts
@@ -1,8 +1,16 @@
 
 import { DashboardService } from "../../dashboard-viewer/services/DashboardService";
-import { IDashboardContent, IMetricFilter } from "../IDashboard";
+import { IDashboardContent } from "../IDashboard";
 import { ITile } from "../ITile";
 import { ITileContent } from "./ITileContent";
+
+type Operator = 'Equals' | 'NotEquals';
+
+interface IYamlMetricFilter {
+    attribute: string;
+    operator: Operator;
+    values: string[];
+}
 
 export class MetricsContent implements ITileContent {
     parent?: ITile;
@@ -12,7 +20,7 @@ export class MetricsContent implements ITileContent {
     public namespace!: string;
     public name!: string;
     public aggregation!: string;
-    public filters!: {key: string, value: string}[];
+    public filters!: IYamlMetricFilter[];
 
     copy(tileContent: ITileContent): void {
         this.resource = (tileContent as MetricsContent).resource;
@@ -79,16 +87,19 @@ export class MetricsContent implements ITileContent {
             dashboardContent.options.chart.filterCollection = {
                 filters: this.filters.map(f => {
                     return {
-                        key: f.key,
-                        operator: 0,
-                        values: [
-                            f.value
-                        ]
+                        key: f.attribute,
+                        operator: this.translateOperatorFromYaml(f.operator),
+                        values: f.values
                     }
                 })
             }
         }
         return dashboardContent;
+    }
+
+    translateOperatorFromYaml(operator?: Operator): number {
+        if (operator === 'NotEquals') return 1;
+        return 0;
     }
 
     loadFromPartContent(partContent: IDashboardContent): void {
